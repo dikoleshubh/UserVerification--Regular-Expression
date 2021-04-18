48214e5dcb5e3b402bb4ed8840b63955956da0b7
dikoleshubh <dikole.shubh@gmail.com>
2021-04-18 13:54:23 +0530

    USER NAME VERIFICATION

---
+++ Execution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReX
+{
+    class name
+    {
+        static string Name = "^[A-Z]{1}[a-z]{2,}$";
+        Regex regexName = new Regex(Name);
+
+        public void firstname ( string firstname)
+        {
+            if(regexName.IsMatch(firstname))
+            {
+                Console.WriteLine("First Name Is  Valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name Is  Invalid");
+            }
+        }
+        public void Lastname(string Lastname)
+        {
+            if (regexName.IsMatch(Lastname))
+            {
+                Console.WriteLine("Last Name Is  Valid");
+            }
+            else
+            {
+                Console.WriteLine("Last Name Is  Invalid");
+            }
+        }
+
+    }
+
+}
