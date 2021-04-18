0b1af488b730e7d7c00aa4c78e97d86897d6ba42
dikoleshubh <dikole.shubh@gmail.com>
2021-04-18 13:51:46 +0530

    USER VERIFICATION BY Regular Expression

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ReX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           ////
+            name ver = new name();
+            MailVerification ver1 = new MailVerification();
+            Password ver2 = new Password();
+
+            Console.WriteLine("------Welcome to Regular expression----- ");
+            
+            Console.WriteLine("Enter your First name");
+            string firstname = Console.ReadLine();
+            ver.firstname(firstname);
+
+            Console.WriteLine("Enter your last name");
+            string Lastname = Console.ReadLine();
+            ver.Lastname(Lastname);
+
+            Console.WriteLine("Enter your EmailID");
+            string emailID = Console.ReadLine();
+            ver1.verificationemail(emailID);
+
+            Console.WriteLine("Enter your Password");
+            string passwordID = Console.ReadLine();
+            ver2.passwordvarification(passwordID);
+
+
+
+
+
+
+        }
+    }
+}
