b91681ad69d7a7f6457c6ce001ec5bbfc7fabe43
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-04-20 09:46:21 +0530

    Conditions to be checked for Ideal Name. i.e. UPPER CASE & LOWER CASE Min-3 char

--- Execution.cs
+++ Execution.cs
@@ -5,14 +5,14 @@ using System.Text.RegularExpressions;
 
 namespace ReX
 {
-    class Name
+    class Name     ////User Name verification   
     {
-        static string Name = "^[A-Z]{1}[a-z]{2,}$";
-        Regex regexName = new Regex(Name);
+        static string Name = "^[A-Z]{1}[a-z]{3}$"; //// Condtions to be checked for Ideal Name. i.e. UPPER CASE & LOWER CASE
+        Regex regexName = new Regex(Name); //// Regex Declaration
 
         public void firstname ( string firstname)
         {
-            if(regexName.IsMatch(firstname))
+            if(regexName.IsMatch(firstname)) //// First Name Validation Check
             {
                 Console.WriteLine("First Name Is  Valid");
             }
@@ -23,7 +23,7 @@ namespace ReX
         }
         public void Lastname(string Lastname)
         {
-            if (regexName.IsMatch(Lastname))
+            if (regexName.IsMatch(Lastname)) //// Last Name Validation Check
             {
                 Console.WriteLine("Last Name Is  Valid");
             }
