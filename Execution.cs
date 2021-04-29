0152bf9e63ac06706583cb41264fc4408d597051
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-04-29 08:38:16 +0530

    USERNAME VALIDATION

---
+++ Execution.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReX
+{
+    class Name     ////User Name verification   
+    {
+        static string Name = "^[A-Z]{1}[a-z]{3}$"; //// Condtions to be checked for Ideal Name. i.e. UPPER CASE & LOWER CASE
+        Regex regexName = new Regex(Name); //// Regex Declaration
+
+        public void firstname ( string firstname)
+        {
+            if(regexName.IsMatch(firstname)) //// First Name Validation Check
+            {
+                Console.WriteLine("First Name Is Valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name Is Invalid");
+            }
+        }
+        public void Lastname(string Lastname)
+        {
+            if (regexName.IsMatch(Lastname)) //// Last Name Validation Check
+            {
+                Console.WriteLine("Last Name Is  Valid");
+            }
+            else
+            {
+                Console.WriteLine("Last Name Is  Invalid");
+            }
+        }
+
+    }
+
+}
